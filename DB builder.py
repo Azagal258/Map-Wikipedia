0263288bc5623d85adadb503cd3288f61cf10621
Azagal258 <azagal.minecraft@gmail.com>
2024-06-30 00:15:20 +0200

    Connect and inputs to DB

--- DB builder.py
+++ DB builder.py
@@ -10,7 +10,7 @@ import mysql.connector
 
 ### Nodes processing ###
 
-def extract_titles(document_xml: str) -> list:
+def extract_infos(document_xml: str) -> list:
     """ 
     Extract all usefull infos (here : IDs, Namespaces, Titles and Revision IDs) and contatenates them in a list of tuples
     in view of inputting them into the database
@@ -20,13 +20,11 @@ def extract_titles(document_xml: str) -> list:
 
     Outputs :
     - list of tuples : list a list of tuple as (id, ns, title, revision id)
-
     """
-    print("Nodes processing started")
     out_tuples = []
     tree = ET.parse(document_xml)
     root = tree.getroot()
-    # Jinsoul: maybe try merge these two "finds" as they're expensive operations
+
     for page in root.findall('{http://www.mediawiki.org/xml/export-0.10/}page'): #'for variable in root.iter('{le truc xmlns}nom_de_la_balise')' 
 
         id = page.find('{http://www.mediawiki.org/xml/export-0.10/}id')
@@ -35,20 +33,46 @@ def extract_titles(document_xml: str) -> list:
         rev_id = page.find('{http://www.mediawiki.org/xml/export-0.10/}revision/{http://www.mediawiki.org/xml/export-0.10/}id')
 
         out_tuples.append((id.text, ns.text, titles.text, rev_id.text))
-    print(out_tuples)
+
     return out_tuples
 
-def make_db(titles: list):
+def init_db(infos: list):
     """ 
-    Make a csv files of all the titles with them as label and lowercase as id 
-    
-    Parameters :
-    - titles (list) : the path to the file
-    - nodes_file
+    Connects to a pre-established database with data from a .env file containing all connection informations.
+    Then adds all data given in input
 
-    Outputs :
-    - a .csv file
+    Parameters :
+    - infos (list) : a list of tuples containing values to input
     """
+    load_dotenv()
+    MYSQL_HOST = os.getenv('MYSQL_HOST')
+    MYSQL_USER = os.getenv('MYSQL_USER')
+    MYSQL_PASSWORD = os.getenv('MYSQL_PASSWORD')
+    MYSQL_DATABASE = os.getenv('MYSQL_DATABASE')
+
+    # Establish a connection to the MySQL database and create cursor
+    conn = mysql.connector.connect(
+        host=MYSQL_HOST,
+        user=MYSQL_USER,
+        password=MYSQL_PASSWORD,
+        database=MYSQL_DATABASE
+    )
+    cursor = conn.cursor()
+
+    #First querry
+    insert_first_set = """
+    INSERT INTO artinfo (ai_id_general, ai_namespace, ai_title, ai_id_revision)
+    VALUES (%s, %s, %s, %s)
+    """
+
+    #Inputs
+    cursor.executemany(insert_first_set, infos)
+
+    #Closes connection
+    conn.commit()
+    cursor.close()
+    conn.close()
+
     print("Database created")
 
 ######## Magic Land ########
@@ -59,5 +83,5 @@ document_xml = "test_article.xml"
 
 
 ### Code ###
-titles = extract_titles(document_xml)
-make_db(titles)
+bulk = extract_infos(document_xml)
+init_db(bulk)
